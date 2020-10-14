4d589b08387123251f615b0ed5ed8ead10c56d2c
armsultan <armsultan@gmail.com>
2020-10-14 16:44:43 -0600

    using njs

--- etc/nginx/conf.d/authrouter.js
+++ etc/nginx/conf.d/authrouter.js
@@ -1,11 +1,44 @@
-function get_type(r) {
-    r.subrequest('/env_endpoint')
-    .then(reply => JSON.parse(reply.responseBody))
-    .then(json => { 
-        r.headersOut['type'] = json.environment; 
-        r.return(200);
-    })
-    .catch(e => r.return(500, e));
- }
+function get_invalid(r) {
 
-export default {get_type};
+    var regex = /wsse:InvalidField/;
+    var body = r.requestBody;
+    // return regex.test(body);
+
+    if (regex.test(body)) {
+        return "1";
+    }
+    else {
+        return "0";
+    }
+}
+
+function get_username(r) {
+    var body = r.requestBody;
+    var regex = /<wsse:Username>(?<u>.*)<\/wsse:Username>/;
+    return regex.exec(body)[1];
+
+}
+
+function get_password(r) {
+    var body = r.requestBody;
+    var regex = /<wsse:Password.*>(?<p>.*)<\/wsse:Password>/;
+    return regex.exec(body)[1];
+}
+
+function get_type(r) {
+
+    r.subrequest('/env_endpoint', JSON.stringify({ created: "2020-08-12T17:46:35.942Z", nonce: 1, origin: "localhost", password: r.variables.password, requestedMethod: "", requestedUri: "", username: r.variables.username }))
+        .then(reply => {
+            r.error(reply.responseBody);
+            var json = JSON.parse(reply.responseBody);
+            r.error(json.type);
+            r.headersOut['type'] = json.type;
+            r.return(200);
+        })
+        .catch(e => {
+            r.error(e);
+            r.return(500, e);
+        });
+}
+
+export default { get_invalid, get_username, get_password, get_type };
